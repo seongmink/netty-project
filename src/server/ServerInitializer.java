c76363109d6e11a0ded05499892901bee5baa11e
KSM <sminggo5@naver.com>
2020-11-02 15:26:38 +0900

    Add ServerHandler, ServerInitializer

---
+++ src/server/ServerInitializer.java
@@ -0,0 +1,18 @@
+package src.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.bytes.ByteArrayDecoder;
+import io.netty.handler.codec.bytes.ByteArrayEncoder;
+
+public class ServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline p = ch.pipeline();
+        p.addLast(new ByteArrayDecoder());
+        p.addLast(new ByteArrayEncoder());
+        p.addLast(new ServerHandler());
+    }
+}
--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,5 +1,38 @@
 package src.server;
 
-public class ServerMain {
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
+public class ServerMain {
+    public static void main(String[] args) {
+        // 클라이언트의 연결을 수락하는 부모 스레드 그룹
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+
+        // 연결된 클라이언트 소켓으로부터 데이터 입출력(I/O) 및 이벤트처리를 담당하는 자식 쓰레드 그룹
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            // ServerBootstrap 클래스를 사용하면 서버에서 Channel을 직접 세팅 할 수 있음
+            ServerBootstrap sb = new ServerBootstrap();
+            sb.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class) // 새로운 채널을 객체화 하는 클래스 지정
+                    .childHandler(new ServerInitializer()).option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            int port = 8888;
+            ChannelFuture f = sb.bind(port).sync();
+            System.out.println("Server Start : " + port);
+            f.channel().closeFuture().sync();
+            f.addListener(ChannelFutureListener.CLOSE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
 }
