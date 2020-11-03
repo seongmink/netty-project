291cc584d2202f3d74ba4186587558994262d8de
KSM <sminggo5@naver.com>
2020-11-03 11:18:29 +0900

    Fix file save / server

--- src/server/ServerHandler.java
+++ src/server/ServerHandler.java
@@ -1,7 +1,10 @@
 package src.server;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -13,14 +16,7 @@ public class ServerHandler extends ChannelHandlerAdapter {
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 		System.out.println("Server channelRead!");
-
-		// TODO : 목표
-		// 바이트를 변환 시킴 ( length / 구분코드 / data / file )
-		// length = 8 byte( 2(구분코드) + data의 길이)
-		// 구분코드 = FI
-		// data = '\n'으로 split후 구조 나눠서 진행
-		// file = 바이트를 파일로 변환
-
+		
 		byte[] bytes = (byte[]) msg;
 
 		// TODO : LENGTH = 첫 8 byte( 2(구분코드) + data의 길이)
@@ -57,55 +53,46 @@ public class ServerHandler extends ChannelHandlerAdapter {
 		String saveDir = st.nextToken().split("=")[1];
 		System.out.println("saveDir = " + saveDir);
 
-		// TODO : 파일 저장하는 로직
+		// TODO : 파일 저장
 
 		byte[] file = new byte[Integer.parseInt(fileSize)];
-		for (int i = LENGTH + CODE_LENGTH + length; i < bytes.length; i++) {
-			file[i-LENGTH-CODE_LENGTH-length] = bytes[i];
+		for (int i = LENGTH + length; i < bytes.length; i++) {
+			file[i-LENGTH-length] = bytes[i];
 		}
 
-//
-//		System.out.println("data = " + data.toString());
-//
-//        ByteBuf in = (ByteBuf) msg;
-//        byte[] b = new byte[in.readableBytes()];
-//        in.getBytes(0, b, 0, in.readableBytes());
-//        String s = new String(b);
-//        //System.out.println("Server received: " + ByteBufUtil.hexDump(in));
-//        System.out.println("Server received: " + s);
-//        ctx.write(in);
+		// TODO : Client로 result 전송
+		byte[] result = new byte[LENGTH + saveDir.length()];
+		byte[] resultLength = String.valueOf(LENGTH + saveDir.length()).getBytes();
+		System.arraycopy(resultLength, 0, result, LENGTH - resultLength.length, resultLength.length);
 
-//        ByteBuf buf = (ByteBuf) msg;
-//
-//        int length = buf.readableBytes();
-//        System.out.println("length = " + length);
-//        byte[] read = new byte[length];
-//
-//        for (int i = 0; i < read.length; i++) {
-//            read[i] = buf.getByte(i);
-//        }
-//
-//        ByteArrayInputStream bis = new ByteArrayInputStream(read);
-//        ObjectInputStream ois = new ObjectInputStream(bis);
-//        System.out.println(ois.toString());
+		byte[] resultData = saveDir.getBytes();
+		System.arraycopy(resultData, 0, result, LENGTH, resultData.length);
 
-//        int totalLength = buf.readableBytes();
-//
-//        byte[] length = new byte[8];
-//        for (int i = 0; i < length.length; i++) {
-//            length[i] = buf.getByte(i);
-//        }
-//
-//        byte[] code = new byte[2];
-//        for (int i = length.length; i < length.length + code.length; i++) {
-//            code[i] = buf.getByte(i);
-//        }
+		File saveFile = new File(saveDir);
+		FileOutputStream fos;
+
+		try {
+			fos = new FileOutputStream(saveFile);
+			fos.write(file);
+			fos.close();
+
+			result[8] = 'O';
+			result[9] = 'K';
+		} catch (Exception e) {
+			result[8] = 'N';
+			result[9] = 'O';
+			e.printStackTrace();
+		} finally {
+			ByteBuf byteBuf = Unpooled.wrappedBuffer(result);
+			ctx.writeAndFlush(byteBuf);
+			System.out.println("Arrays.toString(result) = " + Arrays.toString(result));
+		}
 	}
 
 	@Override
 	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
 		System.out.println("Server channelReadComplete!");
-		//ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+		ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
 	}
 
 	@Override
