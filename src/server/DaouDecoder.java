28998480ad666990742c0e6434ccb00daa8aa0ed
KSM <sminggo5@naver.com>
2020-11-05 11:20:22 +0900

    Fix decoder use to ByteBuf

--- src/server/DaouDecoder.java
+++ src/server/DaouDecoder.java
@@ -1,11 +1,9 @@
 package src.server;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class DaouDecoder extends ByteToMessageDecoder {
@@ -15,41 +13,49 @@ public class DaouDecoder extends ByteToMessageDecoder {
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
 
-        if(!in.isReadable())
+        if(!in.isReadable() || in.readableBytes() < LENGTH)
             return;
 
         String lengthTemp = "";
         for (int i = 0; i < LENGTH; i++) {
-            if(in.getByte(i) != 0)
-                lengthTemp += (char)in.getByte(i);
+            char nowByte = (char) in.readByte();
+            if (nowByte != 0)
+                lengthTemp += nowByte;
+        }
+
+        if(CODE_LENGTH > in.readableBytes()){
+            in.resetReaderIndex();
+            return;
+        } else {
+            in.skipBytes(2); // 구분 코드
         }
 
         int dataLength = Integer.parseInt(lengthTemp) - 2;
-
-        if(dataLength > in.readableBytes())
-            return;
-        StringBuffer sf = new StringBuffer();
-        for (int i = LENGTH + CODE_LENGTH; i < LENGTH + CODE_LENGTH + dataLength; i++) {
-            sf.append((char)in.getByte(i));
-        }
-
-        String[] arrD = sf.toString().split("=");
-        int fileSize = Integer.parseInt(arrD[4].split("\n")[0]);
-        if((in.readableBytes() < LENGTH + CODE_LENGTH + dataLength + fileSize)) {
+        if (dataLength > in.readableBytes()) {
             in.resetReaderIndex();
             return;
         }
 
-        byte[] file;
+        StringBuffer sf = new StringBuffer();
+        for (int i = 0; i < dataLength; i++) {
+            sf.append((char)in.readByte());
+        }
 
+        int fileSize = Integer.parseInt(sf.toString().split("\n")[3].split("=")[1]);
+        if(in.readableBytes() < fileSize) {
+            in.resetReaderIndex();
+            return;
+        }
+
+        byte[] msg;
         if(in.hasArray()) { // heap 버퍼일 경우
-            file = in.array();
+            msg = in.array();
         } else { // direct 버퍼일 경우(네티 4.1부터 기본 버퍼타입이 direct)
-            file = new byte[in.readableBytes()];
-            in.getBytes(in.readerIndex(), file);
+            msg = new byte[in.readableBytes()];
+            in.getBytes(0, msg);
         }
 
         in.clear();
-        out.add(file);
+        out.add(msg);
     }
 }
