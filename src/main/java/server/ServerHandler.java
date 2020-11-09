189b35570f1b7f62c2ef1ebb65ecc37ef631c02b
KSM <sminggo5@naver.com>
2020-11-09 11:43:30 +0900

    Fix log console to file

--- src/main/java/server/ServerHandler.java
+++ src/main/java/server/ServerHandler.java
@@ -3,18 +3,21 @@ package server;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+@Slf4j
 public class ServerHandler extends ChannelHandlerAdapter {
     private final int LENGTH = 8;
     private final int CODE_LENGTH = 2;
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		System.out.println("Server channelRead!");
+		log.info("--------------------------------------------------");
+		log.info("Server channelRead");
 
 		ByteBuf buf = (ByteBuf) msg;
 
@@ -24,14 +27,14 @@ public class ServerHandler extends ChannelHandlerAdapter {
 			lengthTemp += (char) buf.readByte();
 		}
 		int dataLength = Integer.parseInt(lengthTemp.trim()) - 2;
-		System.out.println("length = " + dataLength);
+		log.info("length = " + dataLength);
 
 		// TODO : 구분 코드
 		String code = "";
 		for (int i = 0; i < CODE_LENGTH; i++) {
 			code += (char) buf.readByte();
 		}
-		System.out.println("code = " + code);
+		log.info("code = " + code);
 
 		// TODO : DATA
 		byte[] dataByte = new byte[dataLength];
@@ -41,15 +44,15 @@ public class ServerHandler extends ChannelHandlerAdapter {
 		StringTokenizer st = new StringTokenizer(new String(dataByte), "\n");
 
 		String cmd = st.nextToken().split("=")[1];
-		System.out.println("cmd = " + cmd);
+		log.info("cmd = " + cmd);
 		String fileName = st.nextToken().split("=")[1];
-		System.out.println("fileName = " + fileName);
+		log.info("fileName = " + fileName);
 		String orgFileName = st.nextToken().split("=")[1];
-		System.out.println("orgFileName = " + orgFileName);
+		log.info("orgFileName = " + orgFileName);
 		String fileSize = st.nextToken().split("=")[1];
-		System.out.println("fileSize = " + fileSize);
+		log.info("fileSize = " + fileSize);
 		String saveDir = st.nextToken().split("=")[1];
-		System.out.println("saveDir = " + saveDir);
+		log.info("saveDir = " + saveDir);
 
 		// TODO : 파일 저장
 		int size = buf.readableBytes();
@@ -94,6 +97,7 @@ public class ServerHandler extends ChannelHandlerAdapter {
 			result[9] = 'O';
 			System.arraycopy(resultLengthByte, 0, result, LENGTH - resultLengthByte.length, resultLengthByte.length);
 			System.arraycopy(resultData, 0, result, LENGTH + CODE_LENGTH, resultData.length);
+			log.error(e.toString());
 			e.printStackTrace();
 		} finally {
 			ByteBuf byteBuf = Unpooled.wrappedBuffer(result);
@@ -103,14 +107,15 @@ public class ServerHandler extends ChannelHandlerAdapter {
 
 	@Override
 	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-		System.out.println("Server channelReadComplete!");
+		log.info("Server channelReadComplete!");
+		log.info("--------------------------------------------------");
 		ctx.writeAndFlush(Unpooled.EMPTY_BUFFER);
 	}
 
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-		System.out.println("Server exceptionCaught!");
-		cause.printStackTrace();
+		log.error("Server exceptionCaught!");
+		log.error(cause.toString());
 		ctx.close();
 	}
 }
