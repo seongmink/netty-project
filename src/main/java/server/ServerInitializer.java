6751b4dec7ed10d8e7605b1315f06fffd2a70741
KSM <sminggo5@naver.com>
2020-11-10 08:57:29 +0900

    Add ConnectionHandler

--- src/main/java/server/ServerInitializer.java
+++ src/main/java/server/ServerInitializer.java
@@ -3,7 +3,6 @@ package server;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.bytes.ByteArrayDecoder;
 import io.netty.handler.codec.bytes.ByteArrayEncoder;
 
 public class ServerInitializer extends ChannelInitializer<SocketChannel> {
@@ -11,6 +10,7 @@ public class ServerInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline p = ch.pipeline();
+        p.addLast(new ConnectionHandler());
         p.addLast(new DaouDecoder());
         p.addLast(new ServerHandler());
         p.addLast(new ByteArrayEncoder());
