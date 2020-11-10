9ec68342523e47346edd123a9041129f390a17cf
KSM <sminggo5@naver.com>
2020-11-10 14:44:40 +0900

    Fix to test setting

--- src/main/java/client/ClientMain.java
+++ src/main/java/client/ClientMain.java
@@ -9,28 +9,28 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 
 public class ClientMain {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-        try {
-            Bootstrap b = new Bootstrap();
-            // 서버에 연결된 채널 하나만 존재
-            b.group(workerGroup)
-                    // 서버에 연결된 클라이언트 소켓 채널이 어떤 방식으로 동작할지
-                    .channel(NioSocketChannel.class)
-                    .option(ChannelOption.SO_KEEPALIVE, true)
-                    .handler(new ClientInitializer());
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			Bootstrap b = new Bootstrap();
+			// 서버에 연결된 채널 하나만 존재
+			b.group(workerGroup)
+					// 서버에 연결된 클라이언트 소켓 채널이 어떤 방식으로 동작할지
+					.channel(NioSocketChannel.class)
+					.option(ChannelOption.SO_KEEPALIVE, true)
+					.handler(new ClientInitializer());
 
 //      String ip = "172.21.25.36";
-            String ip = "127.0.0.1";
-            int port = 8888;
-            ChannelFuture f = b.connect(ip, port).sync();
-            System.out.println("Client Start : " + ip + ":" + port);
-            f.channel().closeFuture().sync();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            workerGroup.shutdownGracefully();
-        }
-    }
+			String ip = "127.0.0.1";
+			int port = 8888;
+			ChannelFuture f = b.connect(ip, port).sync();
+			System.out.println("Client Start : " + ip + ":" + port);
+			f.channel().closeFuture().sync();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			workerGroup.shutdownGracefully();
+		}
+	}
 }
